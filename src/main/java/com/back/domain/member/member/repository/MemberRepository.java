e5f2046be6b8a57ff82952fc818f0b58017a7d71
crispy <brian3092@naver.com>
2026-07-20 11:05:41 +0900

    MemberRepository QueryDSL Conversion

--- src/main/java/com/back/domain/member/member/repository/MemberRepository.java
+++ src/main/java/com/back/domain/member/member/repository/MemberRepository.java
@@ -3,8 +3,5 @@ package com.back.domain.member.member.repository;
 import com.back.domain.member.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
-public interface MemberRepository extends JpaRepository<Member, Integer> {
-    Optional<Member> findByUsername(String username);
+public interface MemberRepository extends JpaRepository<Member, Integer>, MemberRepositoryCustom {
 }
